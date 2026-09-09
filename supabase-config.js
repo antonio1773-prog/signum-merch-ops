d8a66a687bf0f6995c194453eeb86cebebdd7551
antonio1773-prog <antonio1773@gmail.com>
2026-09-09 19:41:35 -0300

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 window.SIGNUM_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://nurylsjrfvezxdebkdth.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51cnlsc2pyZnZlenhkZWJrZHRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5ODY1NTQsImV4cCI6MjEwNDU2MjU1NH0.WslSm1rqqkjOEBQVPDXG4pnZ3yo-zHT3qlPUwDu5168",
   stateId: "signum-merch-ops"
 };
